0a79643f8e596368dc8e9f750d31dac4fc083c42
BRUNEL <brunelyombi@gmail.com>
2024-01-15 11:34:31 +0100

    Commit

--- register.php
+++ register.php
@@ -5,22 +5,13 @@
     $password = "Gbk719na";
    
     $connection = new mysqli($host, $user, $password, $database);
-    $query= " INSERT INTO `birthday-guests `(`Nom`,`Nombre`) VALUES ('Nom`,`Nombre`)";
-    $connection->query($query);
-    echo($query);
-    if ($connection-> connect_error){
-        echo("error");
-    }   
-
-    else{
-        echo("no error");
-    }
+    
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = $_POST['Nom'];
         $number = $_POST['Nombre'];
 
-        $sql = "INSERT INTO birthday-guests  (Nom, Nombre) VALUES ('$name','$number')";
+        $sql = "INSERT INTO `birthday-guests`  (Nom, Nombre) VALUES ('$name','$number')";
 
         if ($connection->query($sql) == TRUE) {
             echo "New birthday-guests  created successfully";
